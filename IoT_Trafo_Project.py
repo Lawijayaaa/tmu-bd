179b71e953ae365f2792774570e3854dd287bf6f
Lawijayaaa <lawijaya.lutfi@gmail.com>
2024-04-25 14:58:34 +0700

    add main program

---
+++ IoT_Trafo_Project.py
@@ -0,0 +1,22 @@
+import threading
+import time
+import toolboxTMU
+import tkinter as tk
+
+screen = tk.Tk()
+screen.title("TMU Gateway")
+width= screen.winfo_screenwidth() 
+height= screen.winfo_screenheight()
+screen.geometry("%dx%d" % (width, height))
+screen.attributes('-topmost', True)
+screen.configure(background='#17C0EB')
+
+def mainloop():
+    while True:
+        print(1)
+        time.sleep(5)
+
+if __name__ == "__main__":
+    thread1 = threading.Thread(target=mainloop, args=('thread1', 1))
+    thread1.start()
+    screen.mainloop()
