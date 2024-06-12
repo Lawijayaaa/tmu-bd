5cde5fc975873cb8b9594c13715f8924ee2d3a93
Lawijayaaa <lawijaya.lutfi@gmail.com>
2024-06-12 11:14:12 +0700

    Add GPIO input test on SerialTest

--- serialTest.py
+++ serialTest.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python3
 from pymodbus.client import ModbusSerialClient
+import RPi.GPIO as GPIO
+import time
 
 #init modbus device, db, gps
-client = ModbusSerialClient(method='rtu', port='/dev/ttyUSB-RSM', baudrate=19200)
+client = ModbusSerialClient(method='rtu', port='/dev/ttyACM0', baudrate=9600)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(13, GPIO.IN)
+GPIO.setup(22, GPIO.IN)
+GPIO.setup(17, GPIO.IN)
+GPIO.setup(27, GPIO.IN)
 
-getTemp = client.read_holding_registers(0, 4, slave = 7)
-getPLC = client.read_holding_registers(55, 4, slave = 1)
+#Loop
+
+while True:
+    #getTemp = client.read_holding_registers(0, 10, slave = 1)
+    #getElect = client.read_holding_registers(0, 29, slave = 2)
+    #writeRly = client.write_coil(0, False, slave = 3)
+    #print(getTemp.registers)
+    #print(getElect.registers)
+    #print(writeRly)
+    print(GPIO.input(13))
+    print(GPIO.input(17))
+    print(GPIO.input(22))
+    print(GPIO.input(27))
+    print("~~~")
+    time.sleep(2)
+
+    
+getTemp = client.read_holding_registers(0, 10, slave = 1)
 getElect = client.read_holding_registers(0, 29, slave = 2)
-physical2 = client.write_register(501, 0, slave=1)
-
-print(getTemp)
-print(getPLC)
-print(getElect)
+writeRly = client.write_coil(0, False, slave = 3)
+print(getTemp.registers)
+print(getElect.registers)
+print(writeRly)
