b4039bf57c20e8374f0502e3fa5e83ee2e97e77e
Lawijayaaa <lawijaya.lutfi@gmail.com>
2024-06-28 18:01:16 +0700

    return tkinter - check again

--- main.py
+++ main.py
@@ -5,59 +5,40 @@ from toolboxTMU import initTkinter
 
 #init value
 engineName = " Trafo X"
-progStat = [True, True, True]
+progStat = True
 
 #main program
 def mainloop(thread_name, interval):
     global progStat
     #main loop
     while True:
-        for i in range(0, 3):
-            if progStat[i] == True and i == 0:
-                mainScreen.prog1Lbl['text'] = "Running"
-                time.sleep(0.5)
-            elif progStat[i] == False and i == 0:
-                mainScreen.prog1Lbl["text"] = "Stop"
-                time.sleep(0.5)
-            elif progStat[i] == True and i == 1:
-                mainScreen.prog2Lbl['text'] = "Running"
-                time.sleep(0.5)
-            elif progStat[i] == False and i == 1:
-                mainScreen.prog2Lbl["text"] = "Stop"
-                time.sleep(0.5)
-            elif progStat[i] == True and i == 2:
-                mainScreen.prog3Lbl['text'] = "Running"
-                time.sleep(0.5)
-            elif progStat[i] == False and i == 2:
-                mainScreen.prog3Lbl["text"] = "Stop"
-                time.sleep(0.5)
+        while progStat:
+            mainScreen.prog1Lbl['text'] = "Running"
+            time.sleep(0.5)
+        else:
+            mainScreen.prog1Lbl["text"] = "Stop"
+            time.sleep(0.5)
 
 #restart program
-def Restart(progSelect):
+def Restart():
     os.execv(sys.executable, [sys.executable] + ['/main.py'])
 
 #continue program
-def Start(progSelect):
+def Start():
     global progStat
-    progStat[progSelect - 1] = True
+    progStat = True
 
 #pause program
-def Stop(progSelect):
+def Stop():
     global progStat
-    progStat[progSelect - 1] = False
+    progStat = False
 
 if __name__ == "__main__":
     #import tkinter, connect buttons with function
     mainScreen = initTkinter()
-    mainScreen.restartBtn1["command"] = Restart(1)
-    mainScreen.startBtn1["command"] = Start(1)
-    mainScreen.stopBtn1["command"] = Stop(1)
-    mainScreen.restartBtn2["command"] = Restart(2)
-    mainScreen.startBtn2["command"] = Start(2)
-    mainScreen.stopBtn2["command"] = Stop(2)
-    mainScreen.restartBtn3["command"] = Restart(3)
-    mainScreen.startBtn3["command"] = Start(3)
-    mainScreen.stopBtn3["command"] = Stop(3)
+    mainScreen.restartBtn1["command"] = Restart
+    mainScreen.startBtn1["command"] = Start
+    mainScreen.stopBtn1["command"] = Stop
     #threading to multi process between tkinter and main program
     thread1 = threading.Thread(target=mainloop, args=('thread1', 1))
     thread1.start()
